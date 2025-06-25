37d785327ec8306585d5099aada6fa4bdef20d9c
Jake Reck <jakereck@gmail.com>
2025-06-25 09:29:33 -0500

    Added drizzle orm and connected to neon db

---
+++ drizzle.config.ts
@@ -0,0 +1,19 @@
+import 'dotenv/config';
+import { defineConfig } from 'drizzle-kit';
+
+const databaseUrl = process.env.DATABASE_URL!
+
+if (!databaseUrl) {
+  throw new Error("❌ DATABASE_URL is not defined in environment variables.")
+}
+
+export default defineConfig({
+  out: './drizzle/migrations',
+  schema: './drizzle/schema.ts',
+  dialect: 'postgresql',
+  strict: true,
+  verbose: true,
+  dbCredentials: {
+    url: databaseUrl,
+  },
+});
